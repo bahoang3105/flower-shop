33ba52053488d2511d053f40c3284b84e1e8fa57
PCNRepository <phamchinam1998@gmail.com>
2022-12-20 00:34:03 +0700

    fix responsive homepage

--- frontend/components/pages/homepage/collection/index.tsx
+++ frontend/components/pages/homepage/collection/index.tsx
@@ -1,37 +1,56 @@
-import { Col, Image, Row } from 'antd';
+import { Col, Row } from 'antd';
 import PublicImage from 'public/images';
-import { useCallback } from 'react';
 
-export default function FollowerCollection() {
-  const renderCollectionItem = useCallback(
-    (imageSrc: string, className: string) => (
-      <div className={className} style={{ position: 'relative' }}>
-        <Image src={imageSrc} preview={false} />
-        <div className='button-buy-now'>Mua Ngay</div>
-      </div>
-    ),
-    [],
+const BTN_BUYNOW_TYPE_CLASSNAME = {
+  [2]: 'btn-buynow-type-2',
+  [3]: 'btn-buynow-type-3',
+  [4]: 'btn-buynow-type-4',
+};
+
+type BtnBuyNowType = 2 | 3 | 4;
+
+export default function FlowerCollection() {
+  const renderCommonCollectionItem = (imageSrc: string, importClass: string) => (
+    <div className='flower-collection__common-item'>
+      <img className='flower-collection__common-item__image-group' src={imageSrc} alt='' />
+      <div className={`flower-collection__common-item__button-buy-now ${importClass}`}>Mua Ngay</div>
+    </div>
   );
 
   return (
     <>
-      <Row wrap className='collection' gutter={16}>
-        <Col span={24} className='collection__item'>
-          <div className='collection__title center-flex-item'>Bộ sưu tập</div>
-          {renderCollectionItem(PublicImage.banner1, 'collection__item__1')}
+      <Row wrap className='flower-collection' gutter={[16, 16]}>
+        <Col span={24} lg={12}>
+          <div className='flower-collection__first-item'>
+            <div className='flower-collection__first-item__content'>
+              <div className='flower-collection__first-item__content__collection-group center-flex-item'>
+                <div>BỘ SƯU TẬP</div>
+              </div>
+              <div className='flower-collection__first-item__content__image-group'>
+                <img src={PublicImage.banner1} alt='' />
+                <div className='flower-collection__first-item__content__image-group__button-buy-now'>Mua Ngay</div>
+              </div>
+            </div>
+          </div>
         </Col>
-        <Col span={24} className='collection__item'>
-          {renderCollectionItem(PublicImage.banner2, 'collection__item__2')}
-        </Col>
-      </Row>
-      <Row className='collection' gutter={16}>
-        <Col span={24} className='collection__item'>
-          {renderCollectionItem(PublicImage.banner3, 'collection__item__3')}
-        </Col>
-        <Col span={24} className='collection__item'>
-          {renderCollectionItem(PublicImage.banner4, 'collection__item__4')}
-        </Col>
+        {[PublicImage.banner2, PublicImage.banner3, PublicImage.banner4]?.map((src, index) => {
+          return (
+            <Col span={24} lg={12}>
+              {renderCommonCollectionItem(src, BTN_BUYNOW_TYPE_CLASSNAME[(index + 2) as BtnBuyNowType])}
+            </Col>
+          );
+        })}
       </Row>
     </>
   );
+}
+
+{
+  /* <div className='flower-collection__first-item'>
+            <div className='flower-collection__first-item__item'>
+              <div className='flower-collection__first-item__item__title center-flex-item'>Bộ sưu tập</div>
+              <img className='flower-collection__first-item__item__main-img' src={PublicImage.banner1} alt='' />
+              <div className='button-buy-now btn__1'>Mua Ngay</div>
+            </div>
+          </div> */
 }
