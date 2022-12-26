0a898a357a6e18b1793011f62dc73d1136230823
Hoang Nguyen <bahoang3105@gmail.com>
2022-12-26 23:55:42 +0700

    fix 401

--- newbase-fe/hooks/accountGuest/index.ts
+++ newbase-fe/hooks/accountGuest/index.ts
@@ -1,8 +1,14 @@
+import { LOCAL_STORAGE } from "constants/common";
 import { QUERY_KEY } from "constants/routes";
 import { useMutation, useQuery } from "react-query";
 import { deleteAccountGuests, getAccountGuests } from "services/accountGuest";
+import { getToken } from "services/api";
 
 export const useGetAccountGuests = ({ onSuccess, onError, params }: any) => {
+  const token = localStorage.getItem(LOCAL_STORAGE.TOKEN);
+  if (token) {
+    getToken(token);
+  }
   return useQuery(
     [QUERY_KEY.ACCOUNT_GUEST, params],
     () => getAccountGuests(params),
